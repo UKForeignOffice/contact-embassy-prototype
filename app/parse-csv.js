03623f009f7821950fcaab1c0b91e9b25b8f4add
Jonny Wyatt <jonnywyatt2@gmail.com>
2021-03-18 22:36:03 +0000

    Load CSV / render menu levels

---
+++ app/parse-csv.js
@@ -0,0 +1,25 @@
+const Papa = require('papaparse');
+
+module.exports = buffer => {
+  const csv = buffer.toString('utf8')
+  const results = Papa.parse(csv, {
+    header: true
+  });
+  const headers = results.meta.fields;
+  const level2 = {}
+  const level1 = results.data
+    .map(val => {
+      const level1Label = val[headers[0]];
+      const level1Link = val[headers[1]];
+      const level2Label = val[headers[2]];
+      const level3Label = val[headers[3]];
+      const level3Link = val[headers[4]];
+      level2[level1Label] = level2[level1Label] || {}
+      level2[level1Label][level2Label] = level2[level1Label][level2Label] || []
+      level2[level1Label][level2Label].push({ label: level3Label, url: level3Link})
+      return { label: level1Label, link: level1Link }
+    })
+    .filter((val, index, self) => self.findIndex(item => item.label === val.label) === index)
+
+  return { level1, level2 }
+}
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const upload = multer()
+const parseCsv = require('./parse-csv')
 
-// Add your routes here - above the module.exports line
+router.post('/uploads', upload.single('csv'), (req, res, next) => {
+  req.session.data.levels = parseCsv(req.file.buffer)
+  res.redirect('/level1')
+})
 
+router.get('/level2', (req, res, next) => {
+  res.locals.level2 = Object.keys(req.session.data.levels.level2[req.query.label]).map(key => ({
+    label: key,
+    items: req.session.data.levels.level2[req.query.label][key]
+  }))
+  next()
+})
 module.exports = router
