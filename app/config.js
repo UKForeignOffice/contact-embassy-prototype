b5c384574eb7fd73fd33a16ae3cd2c212d0fe987
Jonny Wyatt <jonnywyatt2@gmail.com>
2021-03-19 16:22:42 +0000

    Contact start page

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: '',
+  serviceName: 'Contact an embassy',
 
   // Default port that prototype runs on
   port: '3000',
