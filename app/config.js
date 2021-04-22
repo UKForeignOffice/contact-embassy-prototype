bc819ccf4acabf35b5080212ba5aa8f018aae03b
Jonny Wyatt <jonnywyatt2@gmail.com>
2021-04-22 16:13:39 +0100

    Copy changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Contact an embassy, high commission or consulate in France',
+  serviceName: 'Contact a UK embassy, high commission, or consulate in France',
 
   // Default port that prototype runs on
   port: '3000',
